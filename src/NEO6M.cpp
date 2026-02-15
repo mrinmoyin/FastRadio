b417840c5cd5c8166119c3556fcc887de4b480e8
MRINMOY MONDAL <mm2922894@gmail.com>
2026-02-15 16:54:26 +0530

    Update NEO6M
    Initialized NEO6M

--- src/NEO6M.cpp
+++ src/NEO6M.cpp
@@ -3,14 +3,27 @@
 bool NEO6M::update() {
   std::string gngga[14];
   readLine("$GNGGA,", gngga, 14);
+  // readLine("$GNRMC,", gngga, 14);
   time = gngga[0].c_str();
   lat = gngga[1].c_str();
   lon = gngga[3].c_str();
+  // Serial1.print("Time: ");
+  // Serial1.print(gngga[0].c_str());
+
+  // while (ss.available()) {
+  //   if (ss.find("$GNGGA,")) {
+  //     Serial1.println(ss.readStringUntil('\n'));
+  //     return false;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+
   return true;
 }
 
 void NEO6M::readLine(char *delemeter, std::string *buff, size_t size) {
-  while (ss.available()) {
+  if (ss.available()) {
     if (ss.find(delemeter)) {
       char bytes[100];
       int length = ss.readBytesUntil('\n', bytes, 100);
@@ -20,6 +33,8 @@ void NEO6M::readLine(char *delemeter, std::string *buff, size_t size) {
         ss << bytes[i];
       }
       std::string str = ss.str();
+      Serial1.print("str: ");
+      Serial1.println(str.c_str());
 
       for (int i = 0; i < size; i++) {
         uint8_t delIdx = str.find(",");
--- src/NEO6M.h
+++ src/NEO6M.h
@@ -9,14 +9,14 @@
 
 class NEO6M {
   public:
-    NEO6M(UART ss = Serial): ss(Serial) {};
+    NEO6M(UART &ss = Serial): ss(ss) {};
 
     String time, lat, lon;
 
     bool update();
 
   private: 
-    UART ss;
+    UART &ss;
 
     void readLine(char *delemeter, std::string *buff, size_t size);
 };
